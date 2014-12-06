0f85c379fd3ecbcbb30c3ba78ad82fedc463535a
Matthew Shields <info@matthewshields.co.uk>
2014-12-06 14:55:20 +0000

    update to 1.2
    upon fail to ajax (e.g. 404 error) icon greyed out and shortcut disabled
    window comes into focus when notification clicked

--- intranet-chrome-dist/notification.js
+++ intranet-chrome-dist/notification.js
@@ -10,12 +10,19 @@ var intranetHomepage = 'http://intranet.stickyeyes.com/';
 var notificationPage = 'http://intranet.stickyeyes.com/notifications/';
 var minuteInterval = 5;
 
+var connected = false;
+
 function fetch_feed() {
+    
     $.ajax({
         url: notificationPage,
         type: "GET",
         dataType: "html",
         success: function(data){
+            connected = true;
+            chrome.browserAction.setIcon({
+                path: 'logo.png'
+            });
 
             if($(data).find('.feed-single').length > 0) {
                 $(data).find('.feed-single').each(function() {
@@ -37,7 +44,13 @@ function fetch_feed() {
                     notifyMe(commentReturnedTitle, commentReturnedExcerpt, commentReturnedUrl);
                 });
             }
-        }
+        },
+        error: function() { 
+            connected = false;
+            chrome.browserAction.setIcon({
+                path: 'logo-fail.png'
+            });
+        } 
     });
 }
 
@@ -55,14 +68,22 @@ function notifyMe(title, excerpt, url) {
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({url: intranetHomepage});
-    chrome.browserAction.setBadgeText({ text: "" });
+    if(connected == true) {
+        chrome.tabs.create({url: intranetHomepage});
+        chrome.browserAction.setBadgeText({ text: "" });
+    }
 });
 
 chrome.notifications.onClicked.addListener(function(id, byUser) {
     chrome.tabs.create({url: id});
     chrome.browserAction.setBadgeText({ text: "" });
+    chrome.windows.getCurrent(function(win) {
+        chrome.windows.update(win.id, {focused: true});
+    });
 });
+
+
+fetch_feed();
 
 setInterval( fetch_feed, minuteInterval*60*1000 );
 
